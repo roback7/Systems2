90f1a108d26c7e6a0b91c1a9cb28465345fd72ee
roback.7 <roback.7@osu.edu>
2018-11-18 14:56:14 -0500

    Created userspace

---
+++ userspace.c
@@ -0,0 +1,16 @@
+/*
+** userspace module
+**
+*/
+
+#include <stdlib.h>
+#include <stdio.h> 
+
+int main(){
+	char input[50]; //process to be searched
+
+	puts("Enter a process name:");	
+	gets(input);
+
+	return(0);
+}
