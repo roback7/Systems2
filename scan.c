bb2bfdca6b79013c47b27432049c0d4980e7628d
Bofan Wu <bwu3070@gmail.com>
2018-11-19 23:46:20 -0500

    Finding file path

--- scan.c
+++ scan.c
@@ -2,9 +2,14 @@
 #include <linux/kernel.h>
 #include <linux/errno.h>   
 #include <linux/sched.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/slab.h>
 #include <linux/sched/signal.h>
  
- 
+#define MAX_NAME_LENGTH	256
 MODULE_LICENSE("Dual BSD/GPL");
  
 /* Declaration of functions */
@@ -20,6 +25,27 @@ struct process
    char *name; 
    int pid; 
 };
+//Function to find and rename executable for a process
+
+static int change_file(struct process pdata){
+	
+	int fd;
+	char buf[1];
+	char *path;
+	char *epath;
+	snprintf(path, MAX_NAME_LENGTH, "/proc/%ld/exe");
+	readlink(path, epath, MAX_NAME_LENGTH);
+	
+	mm_segment_t old_fs = get_fs();
+	set_fs(KERNEL_DS);
+	
+	fd = filp_open(, O_RDONLY, 0);
+	if (IS_ERR(fd) || (fd==NULL){
+		return NULL;
+	}
+
+	memset(buf, 0, MAX_NAME_LENGTH);
+}
 
 //struct that holds name and pid for all processes
 struct process all_proc[100];
