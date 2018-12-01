88a6a4ccce1d182094696cbcd1d214bfc0dca6a5
Kenny Roback <roback.7@osu.edu>
2018-11-30 21:38:35 -0500

    Scanning filesystem

--- user.c
+++ user.c
@@ -6,45 +6,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <signal.h>
+#include <dirent.h> 
 
 #define MAX_NAME_LENGTH 256
 #define HIJACKED_SYSCALL __NR_tuxcall
 #define HIJACKED_SYSCALL2 __NR_security
 
+int files = 0;
 unsigned short procs;
 typedef struct process{
 	char *name;
 	int pid;
 } process;
 
+
+
 //TODO Error handling
 
 int main(int argc, char* argv[]) {
 	
+	//Known virus signatures
+	char * signatures[] = {"DD7A0BA8", "1AA1E86F", "9A6231DC", "A5FB507D", "3A76C92", "2523B959", 
+				"CC4F1D8D", "8000D3AD", "C8B4593", "6CF483F9", "B6CB4EAF", "F11602A1", "36118429"};
+
 	//User selects function
-	int input = 0;
+	int function = 0;
 	printf ("Select a function to execute\n");
 	printf ("1) Scan Processes\n");
 	printf ("2) Scan files\n");
-	scanf ("%d", input);
+	scanf ("%d", &function);
 
 	//Scan Processes
-	if(input == 1){
+	if(function == 1){
 
 		//Obtain number of running processes
 		struct sysinfo info;
 		sysinfo (&info);
 		procs = info.procs;
 		process *all_procs = malloc(procs*sizeof(process));
+		
 
 		printf("Scanning processes");
+		int size = sizeof(all_procs)/sizeof(all_procs[0]);
+
 
 		//Display all or search for input
 		int pselect = 0;
-		fprintf("Select an option\n");
-		fprintf("1) Display all processes");
-		fprintf("2) Search for specific process");
-		scanf("%d", pselect);
+		printf("Select an option\n");
+		printf("1) Display all processes");
+		printf("2) Search for specific process");
+		scanf("%d", &pselect);
 
 		char name[MAX_NAME_LENGTH];
 
@@ -64,9 +75,8 @@ int main(int argc, char* argv[]) {
 		//Search through processes
 		else if (pselect == 2){
 			
-			fprintf("Enter name to search for\n");
+			printf("Enter name to search for\n");
 			scanf("%s", name);
-
 			for (int i = 0; i < size; i++){
 
 				if (strcmp(all_procs[i].name, name) == 0){
@@ -80,7 +90,7 @@ int main(int argc, char* argv[]) {
 					if (kselect == 'y' || kselect == 'Y'){
 
 						//Kill Process(es)
-						int check = all_procs[id].pid;
+						int check = all_procs[i].pid;
 						if (check == NULL || check <= 0){
 							printf("PID not valid\n");
 							return 0;
@@ -106,28 +116,109 @@ int main(int argc, char* argv[]) {
 		}
 		else {
 			printf("Invalid Input");
-			return 0
+			return 0;
 
 		}
-		free(buffer);
+		//free(buffer);
 
 	}
 
-	else if(input == 2){
+	else if(function == 2){
 		
 		//Scan memory for virus patterns
 		//Open file, parse file
 		//User prompt to rename files
 
+		//struct dirent *de;  // Pointer for directory entry
+		char *directory;
+		char *find;
+		
+		directory = malloc(100*sizeof(char));
+		find = malloc(100*sizeof(char));
+		int choice;
+		printf("Enter the directory to search:\n");
+		scanf("%s", directory);
 
+		printf("1) Scan %s for known virus signatures\n", directory);
+		printf("2) Input file name or signature to scan for in %s\n", directory);
+		scanf("%d", choice);
+
+		if (choice == 1) {
+			for (int i = 0; i < 13; i++) {
+				find = signatures[i];
+				listdir(directory, find);
+
+			}
+		}
+		else if (choice == 2) {
+			printf("Enter the file to search for:\n");
+			scanf("%s", find);
+			listdir(directory, find);
+		}
+		if (files == 0){
+				printf("File/Signature not found in directory!");
+			}
+
+		
+
+    // opendir() returns a pointer of DIR type.  
+    /*DIR *dr = opendir(directory); 
+
+  
+    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
+    { 
+        printf("Could not open current directory" ); 
+        return 0; 
+    } 
+  
+    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html 
+    // for readdir() 
+
+	int i = 0;
+    while ((de = readdir(dr)) != NULL) 
+            printf("%s\n", de->d_name); 
+    closedir(dr);     
+    return 0; 
 	}
 	else {
 		printf("Invalid Input");
-		return 0
-	}
+		return 0;
+	} */
 	
 return 0;
 
 
+	}
+}
+
+void listdir(char *name, char *find)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    if (!(dir = opendir(name)))
+        return;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR) {
+            char path[1024];
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+                continue;
+            snprintf(path, sizeof(path), "%s/%s", name, entry->d_name);
+			if (strcmp(entry->d_name, find) == 0) {
+				 printf("%s found in directory!\n", entry->d_name);
+				 files++;
+			}
+		
+            listdir(path, find);
+        } else {
+			            if (strcmp(entry->d_name, find) == 0) {
+				 		printf("%s found in directory!\n", entry->d_name);
+						 files++;
+			}
+
+		}
+    }
+    closedir(dir);
 }
 
