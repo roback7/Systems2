87a34ebe4e6675f1c52bd17c7c424a630b26b14c
roback7 <42950649+roback7@users.noreply.github.com>
2018-12-03 00:56:02 -0500

    Revert "Revert "Merge branch 'oops' of https://github.com/roback7/Systems2""
    This reverts commit 297095945d83b8662f20a70f0fc1cbafffb4dd40.

--- .scan2.mod.o.cmd
+++ .scan2.mod.o.cmd
@@ -1,9 +1,13 @@
-cmd_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := gcc -Wp,-MD,/home/ken/Desktop/newbranch/Systems2/.scan.mod.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/5/include  -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -Iubuntu/include  -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -fno-PIE -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -DRETPOLINE -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -O2 --param=allow-store-data-races=0 -DCC_HAVE_ASM_GOTO -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -pg -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init  -DKBUILD_BASENAME='"scan.mod"'  -DKBUILD_MODNAME='"scan"' -DMODULE  -c -o /home/ken/Desktop/newbranch/Systems2/scan.mod.o /home/ken/Desktop/newbranch/Systems2/scan.mod.c
+cmd_/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o := gcc -Wp,-MD,/home/kenny/Desktop/otherbranch/Systems2/.scan2.mod.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/8/include  -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -Iubuntu/include  -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -fno-PIE -DCC_HAVE_ASM_GOTO -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -funit-at-a-time -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_FXSAVEQ=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_CRC32=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -pipe -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -DRETPOLINE -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-int-in-bool-context -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -pg -mfentry -DCC_USING_FENTRY -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -fmacro-prefix-map=./= -Wno-packed-not-aligned  -DKBUILD_BASENAME='"scan2.mod"' -DKBUILD_MODNAME='"scan2"' -DMODULE  -c -o /home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o /home/kenny/Desktop/otherbranch/Systems2/scan2.mod.c
 
-source_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := /home/ken/Desktop/newbranch/Systems2/scan.mod.c
+source_/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o := /home/kenny/Desktop/otherbranch/Systems2/scan2.mod.c
 
-deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
+deps_/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o := \
     $(wildcard include/config/module/unload.h) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
   include/linux/compiler_types.h \
     $(wildcard include/config/enable/must/check.h) \
     $(wildcard include/config/enable/warn/deprecated.h) \
@@ -27,6 +31,7 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/event/tracing.h) \
     $(wildcard include/config/ftrace/mcount/record.h) \
     $(wildcard include/config/constructors.h) \
+    $(wildcard include/config/function/error/injection.h) \
     $(wildcard include/config/strict/module/rwx.h) \
   include/linux/list.h \
     $(wildcard include/config/debug/list.h) \
@@ -48,6 +53,7 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   include/uapi/linux/posix_types.h \
   include/linux/stddef.h \
   include/uapi/linux/stddef.h \
+  include/linux/compiler_types.h \
   arch/x86/include/asm/posix_types.h \
     $(wildcard include/config/x86/32.h) \
   arch/x86/include/uapi/asm/posix_types_64.h \
@@ -55,6 +61,7 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   include/linux/poison.h \
     $(wildcard include/config/illegal/pointer/value.h) \
     $(wildcard include/config/page/poisoning/zero.h) \
+  include/linux/const.h \
   include/uapi/linux/const.h \
   include/linux/kernel.h \
     $(wildcard include/config/preempt/voluntary.h) \
@@ -63,12 +70,12 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/prove/locking.h) \
     $(wildcard include/config/arch/has/refcount.h) \
     $(wildcard include/config/lock/down/kernel.h) \
+    $(wildcard include/config/lock/down/mandatory.h) \
     $(wildcard include/config/panic/timeout.h) \
-  /usr/lib/gcc/x86_64-linux-gnu/5/include/stdarg.h \
+  /usr/lib/gcc/x86_64-linux-gnu/8/include/stdarg.h \
   include/linux/linkage.h \
   include/linux/stringify.h \
   include/linux/export.h \
-    $(wildcard include/config/have/underscore/symbol/prefix.h) \
     $(wildcard include/config/modversions.h) \
     $(wildcard include/config/module/rel/crcs.h) \
     $(wildcard include/config/trim/unused/ksyms.h) \
@@ -81,13 +88,13 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/stack/validation.h) \
     $(wildcard include/config/kasan.h) \
   arch/x86/include/asm/barrier.h \
-    $(wildcard include/config/x86/ppro/fence.h) \
   arch/x86/include/asm/alternative.h \
   arch/x86/include/asm/asm.h \
   arch/x86/include/asm/nops.h \
     $(wildcard include/config/mk7.h) \
     $(wildcard include/config/x86/p6/nop.h) \
   include/asm-generic/barrier.h \
+  include/linux/kasan-checks.h \
   include/linux/bitops.h \
   arch/x86/include/asm/bitops.h \
     $(wildcard include/config/x86/cmov.h) \
@@ -104,18 +111,17 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/x86/cmpxchg64.h) \
     $(wildcard include/config/x86/use/3dnow.h) \
     $(wildcard include/config/matom.h) \
-    $(wildcard include/config/x86/5level.h) \
     $(wildcard include/config/paravirt.h) \
   arch/x86/include/asm/disabled-features.h \
     $(wildcard include/config/x86/intel/mpx.h) \
     $(wildcard include/config/x86/intel/umip.h) \
     $(wildcard include/config/x86/intel/memory/protection/keys.h) \
+    $(wildcard include/config/x86/5level.h) \
     $(wildcard include/config/page/table/isolation.h) \
   include/asm-generic/bitops/const_hweight.h \
   include/asm-generic/bitops/le.h \
   arch/x86/include/uapi/asm/byteorder.h \
   include/linux/byteorder/little_endian.h \
-    $(wildcard include/config/cpu/big/endian.h) \
   include/uapi/linux/byteorder/little_endian.h \
   include/linux/swab.h \
   include/uapi/linux/swab.h \
@@ -179,8 +185,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/amd/mem/encrypt.h) \
   include/linux/thread_info.h \
     $(wildcard include/config/thread/info/in/task.h) \
-    $(wildcard include/config/debug/stack/usage.h) \
-    $(wildcard include/config/debug/kmemleak.h) \
     $(wildcard include/config/have/arch/within/stack/frames.h) \
     $(wildcard include/config/hardened/usercopy.h) \
   include/linux/bug.h \
@@ -191,16 +195,24 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/bug.h) \
     $(wildcard include/config/generic/bug/relative/pointers.h) \
   include/linux/restart_block.h \
-    $(wildcard include/config/compat.h) \
+  include/linux/time64.h \
+    $(wildcard include/config/64bit/time.h) \
+  include/linux/math64.h \
+    $(wildcard include/config/arch/supports/int128.h) \
+  arch/x86/include/asm/div64.h \
+  include/asm-generic/div64.h \
+  include/uapi/linux/time.h \
   arch/x86/include/asm/current.h \
   arch/x86/include/asm/thread_info.h \
     $(wildcard include/config/vm86.h) \
     $(wildcard include/config/frame/pointer.h) \
+    $(wildcard include/config/compat.h) \
     $(wildcard include/config/ia32/emulation.h) \
   arch/x86/include/asm/page.h \
   arch/x86/include/asm/page_types.h \
     $(wildcard include/config/physical/start.h) \
     $(wildcard include/config/physical/align.h) \
+    $(wildcard include/config/dynamic/physical/mask.h) \
   include/linux/mem_encrypt.h \
     $(wildcard include/config/arch/has/mem/encrypt.h) \
   arch/x86/include/asm/mem_encrypt.h \
@@ -221,9 +233,10 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/x86.h) \
   include/uapi/video/edid.h \
   arch/x86/include/asm/page_64_types.h \
-    $(wildcard include/config/randomize/memory.h) \
+    $(wildcard include/config/dynamic/memory/layout.h) \
     $(wildcard include/config/randomize/base.h) \
   arch/x86/include/asm/kaslr.h \
+    $(wildcard include/config/randomize/memory.h) \
   arch/x86/include/asm/page_64.h \
     $(wildcard include/config/debug/virtual.h) \
     $(wildcard include/config/flatmem.h) \
@@ -237,9 +250,9 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   include/asm-generic/getorder.h \
   arch/x86/include/asm/cpufeature.h \
     $(wildcard include/config/x86/feature/names.h) \
-    $(wildcard include/config/x86/fast/feature/tests.h) \
   arch/x86/include/asm/processor.h \
-    $(wildcard include/config/cc/stackprotector.h) \
+    $(wildcard include/config/kvm.h) \
+    $(wildcard include/config/stackprotector.h) \
     $(wildcard include/config/x86/debugctlmsr.h) \
     $(wildcard include/config/cpu/sup/amd.h) \
     $(wildcard include/config/xen.h) \
@@ -269,11 +282,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/retpoline.h) \
   arch/x86/include/asm/alternative-asm.h \
   arch/x86/include/asm/msr-index.h \
-    $(wildcard include/config/control.h) \
-    $(wildcard include/config/tdp/nominal.h) \
-    $(wildcard include/config/tdp/level/1.h) \
-    $(wildcard include/config/tdp/level/2.h) \
-    $(wildcard include/config/tdp/control.h) \
   arch/x86/include/asm/spinlock_types.h \
     $(wildcard include/config/paravirt/spinlocks.h) \
   include/asm-generic/qspinlock_types.h \
@@ -304,6 +312,7 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   arch/x86/include/asm/cmpxchg.h \
   arch/x86/include/asm/cmpxchg_64.h \
   arch/x86/include/asm/atomic64_64.h \
+  include/asm-generic/atomic-instrumented.h \
   include/linux/tracepoint-defs.h \
   include/linux/atomic.h \
     $(wildcard include/config/generic/atomic64.h) \
@@ -318,10 +327,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   arch/x86/include/asm/orc_types.h \
   include/linux/personality.h \
   include/uapi/linux/personality.h \
-  include/linux/math64.h \
-    $(wildcard include/config/arch/supports/int128.h) \
-  arch/x86/include/asm/div64.h \
-  include/asm-generic/div64.h \
   include/linux/err.h \
   include/linux/irqflags.h \
     $(wildcard include/config/trace/irqflags.h) \
@@ -371,8 +376,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/inline/write/unlock/irq.h) \
     $(wildcard include/config/inline/read/unlock/irqrestore.h) \
     $(wildcard include/config/inline/write/unlock/irqrestore.h) \
-  include/linux/time64.h \
-  include/uapi/linux/time.h \
   include/linux/time32.h \
   include/linux/uidgid.h \
     $(wildcard include/config/multiuser.h) \
@@ -401,12 +404,11 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/flat/node/mem/map.h) \
     $(wildcard include/config/page/extension.h) \
     $(wildcard include/config/no/bootmem.h) \
-    $(wildcard include/config/numa/balancing.h) \
     $(wildcard include/config/deferred/struct/page/init.h) \
+    $(wildcard include/config/numa/balancing.h) \
     $(wildcard include/config/transparent/hugepage.h) \
     $(wildcard include/config/have/memory/present.h) \
     $(wildcard include/config/have/memoryless/nodes.h) \
-    $(wildcard include/config/need/node/memmap/size.h) \
     $(wildcard include/config/have/memblock/node/map.h) \
     $(wildcard include/config/need/multiple/nodes.h) \
     $(wildcard include/config/have/arch/early/pfn/to/nid.h) \
@@ -430,6 +432,7 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/have/arch/nodedata/extension.h) \
     $(wildcard include/config/have/bootmem/info/node.h) \
   include/linux/notifier.h \
+    $(wildcard include/config/tree/srcu.h) \
   include/linux/errno.h \
   include/uapi/linux/errno.h \
   include/linux/mutex.h \
@@ -444,7 +447,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   arch/x86/include/asm/rwsem.h \
   include/linux/srcu.h \
     $(wildcard include/config/tiny/srcu.h) \
-    $(wildcard include/config/tree/srcu.h) \
     $(wildcard include/config/srcu.h) \
   include/linux/rcupdate.h \
     $(wildcard include/config/preempt/rcu.h) \
@@ -554,11 +556,18 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   include/linux/llist.h \
     $(wildcard include/config/arch/have/nmi/safe/cmpxchg.h) \
   include/linux/rbtree.h \
+  include/linux/overflow.h \
   arch/x86/include/uapi/asm/vsyscall.h \
   include/asm-generic/fixmap.h \
+  arch/x86/include/asm/hardirq.h \
+    $(wildcard include/config/kvm/intel.h) \
+    $(wildcard include/config/have/kvm.h) \
+    $(wildcard include/config/x86/thermal/vector.h) \
+    $(wildcard include/config/x86/mce/threshold.h) \
+    $(wildcard include/config/x86/mce/amd.h) \
+    $(wildcard include/config/hyperv.h) \
   arch/x86/include/asm/io_apic.h \
   arch/x86/include/asm/irq_vectors.h \
-    $(wildcard include/config/have/kvm.h) \
     $(wildcard include/config/pci/msi.h) \
   include/linux/topology.h \
     $(wildcard include/config/use/percpu/numa/node/id.h) \
@@ -580,7 +589,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   arch/x86/include/asm/vdso.h \
     $(wildcard include/config/x86/x32.h) \
   include/linux/mm_types.h \
-    $(wildcard include/config/have/cmpxchg/double.h) \
     $(wildcard include/config/have/aligned/struct/page.h) \
     $(wildcard include/config/userfaultfd.h) \
     $(wildcard include/config/have/arch/compat/mmap/bases.h) \
@@ -619,6 +627,9 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
     $(wildcard include/config/ia64.h) \
     $(wildcard include/config/ppc64.h) \
   include/linux/rbtree_latch.h \
+  include/linux/error-injection.h \
+  arch/x86/include/asm/error-injection.h \
+  include/asm-generic/error-injection.h \
   arch/x86/include/asm/module.h \
     $(wildcard include/config/unwinder/orc.h) \
     $(wildcard include/config/m486.h) \
@@ -651,6 +662,6 @@ deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o := \
   include/linux/vermagic.h \
   include/generated/utsrelease.h \
 
-/home/ken/Desktop/newbranch/Systems2/scan.mod.o: $(deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o)
+/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o: $(deps_/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o)
 
-$(deps_/home/ken/Desktop/newbranch/Systems2/scan.mod.o):
+$(deps_/home/kenny/Desktop/otherbranch/Systems2/scan2.mod.o):
--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 ##
-## Makefile for lkm-syscall
+## Makefile for virus scanner**
 ##
-## Made by xsyann
+## Originally made by xsyann, updated by Kenny Roback
 ## Contact <contact@xsyann.com>
 ##
+<<<<<<< HEAD
 ## Started on  Wed Mar 26 13:49:00 2014 xsyann
 ## Last update Fri Apr 18 22:13:46 2014 xsyann
 ##
@@ -11,12 +12,16 @@
 TARGET	= test
 
 obj-m	+= $(TARGET).o
+=======
+## Current version built by Yuan Xiao
+## Contact <xiao.465@osu.edu>
+>>>>>>> e6dfba798e4358753b73d761ac16e27abb078522
 
-CURRENT = $(shell uname -r)
-KDIR	= /lib/modules/$(CURRENT)/build
 
 all:
-	$(MAKE) -C $(KDIR) M=$(PWD) modules
+	cd kernel; make
+	cd userSpace; make
 
 clean:
-	$(MAKE) -C $(KDIR) M=$(PWD) clean
+	cd kernel; make clean
+	cd userSpace; make clean
